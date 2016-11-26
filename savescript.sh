b704d42588c4ec209c2dfebae95db7b6c69d2d9e
root <root@WSSYSCP1.exia>
2016-11-26 13:57:26 +0100

    Changement de répértoires choisies

--- savescript.sh
+++ savescript.sh
@@ -1,7 +1,7 @@
 #Création des archives des sauvegardes dans le dossier var.
 
-save1="tar -cvzf /var/archive.tar.gz /etc/bind/named.conf.local"
-save2="tar -cvzf /var/archive2.tar.gz /etc/bind/db.workshop.fr"
+save1="tar -cvzf /var/archive.tar.gz /etc/bind/"
+save2="tar -cvzf /var/archive2.tar.gz /etc/apache2"
 
 eval $save1
 eval $save2
