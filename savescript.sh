4c984cc67818b9f0726b9f8d28dbd2c01b45af7e
root <root@WSSYSCP1.exia>
2016-11-26 13:58:26 +0100

    Choix du nom des archives.

--- savescript.sh
+++ savescript.sh
@@ -1,7 +1,7 @@
 #Création des archives des sauvegardes dans le dossier var.
 
-save1="tar -cvzf /var/archive.tar.gz /etc/bind/"
-save2="tar -cvzf /var/archive2.tar.gz /etc/apache2"
+save1="tar -cvzf /var/$1.tar.gz /etc/bind/"
+save2="tar -cvzf /var/$2.tar.gz /etc/apache2"
 
 eval $save1
 eval $save2
