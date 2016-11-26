cc170f643e29aed2fbb6b77c5416ebf07b613a13
root <root@WSSYSCP1.exia>
2016-11-26 11:09:12 +0100

    Amélioration du script de sauvegarde.

--- savescript.sh
+++ savescript.sh
@@ -1,5 +1,7 @@
-save1="tar -cvzf /archive.tar.gz /etc/bind/named.conf.local"
-save2="tar -cvzf /archive2.tar.gz /etc/bind/db.workshop.fr"
+#Création des archives des sauvegardes dans le dossier var.
+
+save1="tar -cvzf /var/archive.tar.gz /etc/bind/named.conf.local"
+save2="tar -cvzf /var/archive2.tar.gz /etc/bind/db.workshop.fr"
 
 eval $save1
 eval $save2
